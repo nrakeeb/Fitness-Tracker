46b7af31c2d1dd4e03880e61695ac48b242a869c
nrakeeb <nouha_rakeeb@hotmail.co.uk>
2021-12-16 00:48:24 +0000

    chore: changed db to workout
    Authors: NR

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,50 +2,50 @@
 const router = require("express").Router();
 
 // Import workout model
-const db = require("../models/exercise");
+const { Workout } = require("../models");
 
 // GET Request for getting all workouts
 router.get("/api/workouts", (req, res) => {
-	db.find()
-		.then((dbData) => {
-			res.json(dbData);
-		})
-		.catch((err) => {
-			res.json(err);
-		});
+  Workout.find()
+    .then((dbData) => {
+      res.json(dbData);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 // GET request
 router.get("/api/workouts/range", (req, res) => {
-	db.find()
-		.then((dbData) => {
-			res.json(dbData);
-		})
-		.catch((err) => {
-			res.json(err);
-		});
+  Workout.find()
+    .then((dbData) => {
+      res.json(dbData);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 // POST workout
 router.post("/api/workouts", ({ body }, res) => {
-	db.create(body)
-		.then((dbData) => {
-			res.json(dbData);
-		})
-		.catch((err) => {
-			res.json(err);
-		});
+  Workout.create(body)
+    .then((dbData) => {
+      res.json(dbData);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 // PUT/Update workout
 router.put("/api/workouts/:id", ({ body, params }, res) => {
-	db.findByIdAndUpdate(params.id, { $push: { exercises: body } })
-		.then((dbData) => {
-			res.json(dbData);
-		})
-		.catch((err) => {
-			res.json(err);
-		});
+  Workout.findByIdAndUpdate(params.id, { $push: { exercises: body } })
+    .then((dbData) => {
+      res.json(dbData);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 // Export API routes
