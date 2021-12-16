a74e751f3307dc50cae4170057fd73ad405ac139
nrakeeb <nouha_rakeeb@hotmail.co.uk>
2021-12-16 00:58:08 +0000

    chore: repuired files from route folder
    Author: NR

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
-const router = require("./routes")
+const apiRoutes = require("./routes/api-routes");
+const htmlRoutes = require("./routes/html-routes");
 
 const PORT = process.env.PORT || 3001;
 
@@ -13,14 +14,18 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker",
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  }
+);
 
-app.use(router);
+app.use(apiRoutes, htmlRoutes);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
